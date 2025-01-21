9ac76a844cbf723490fb3865cf5255c79778993b
Aidan Brown <aidanbrown291@u.boisestate.edu>
2025-01-21 14:37:43 -0700

    Created an about page, added CSS rules, added to README

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ To get started, clone this repository and run the following commands:
 ```bash
 npm install
 ```
+
 This will install the necessary dependencies for the project.
 
 ## Development
@@ -27,3 +28,9 @@ To run the tests for the project, run the following command:
 ```bash
 npm test
 ```
+
+## Running The Site
+
+To run the site, you will need to run Live Preview: Start Server. To do this on VS Code, you
+can press ctrl + shift + p, then you should see the command listed. Once you do that, you
+can also use the debugger set to debug website to step through the code on main.js.
--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
  * This function prints the string 'Hello World' to the console
  */
 export function helloWorld() {
-    console.log('Hello World');
+  alert("Hello World");
 }
 
 /**
@@ -22,13 +22,13 @@ export function helloWorld() {
  * @returns {number} The sum of the two numbers
  */
 export function add(num1, num2) {
-    if (num1 === undefined || num2 === undefined) {
-        throw new Error('You must provide two numbers to add');
-    }
-    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-        throw new Error('You must provide two numbers to add');
-    }
-    return num1 + num2;
+  if (num1 === undefined || num2 === undefined) {
+    throw new Error("You must provide two numbers to add");
+  }
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    throw new Error("You must provide two numbers to add");
+  }
+  return num1 + num2;
 }
 
 /**
@@ -36,14 +36,16 @@ export function add(num1, num2) {
  * @returns {string} A joke in the format "setup - punchline"
  */
 export async function fetchRandomJoke() {
-    try {
-        const response = await fetch('https://official-joke-api.appspot.com/random_joke');
-        if (!response.ok) {
-            throw new Error('Failed to fetch a joke');
-        }
-        const joke = await response.json();
-        return `${joke.setup} - ${joke.punchline}`;
-    } catch (error) {
-        throw new Error(error.message);
+  try {
+    const response = await fetch(
+      "https://official-joke-api.appspot.com/random_joke"
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch a joke");
     }
+    const joke = await response.json();
+    return `${joke.setup} - ${joke.punchline}`;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
